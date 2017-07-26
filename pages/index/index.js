e3dcee36540be910655ecec37406bb0c229aaf91
mocsmiths <mocsmiths@github.com>
2017-07-26 21:15:46 +0800

    w-snake
    微信小程序贪吃蛇游戏

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,10 +1,18 @@
-//7.26
 var startX = 0;
 var startY = 0;
 var moveX = 0;
 var moveY = 0;
 var X = 0;
 var Y = 0;
+var snakeHead = {
+  x:0,
+  y:0,
+  color:"#ff0000",
+  w:20,
+  h:20
+}
+var direction = null;
+var snakeDirection = "right";
 Page({
   
   canvasStart:function(e){
@@ -20,13 +28,33 @@ Page({
     Y = moveY - startY;
 
     if(Math.abs(X)>Math.abs(Y) && X>0){
-      console.log("right");
+      direction="right";
     } else if (Math.abs(X) > Math.abs(Y) && X < 0){
-      console.log("left");
+      direction="left";
     } else if (Math.abs(Y) > Math.abs(X) && Y > 0){
-      console.log("DOWN");
+      direction="down";
     } else if (Math.abs(Y) > Math.abs(X) && Y < 0){
-      console.log("UP");
+      direction="top";
     }
+  },
+  canvasEnd:function(){
+    snakeDirection=direction;
+  },
+  onReady:function(){
+    var context = wx.createContext();
+
+    function animate(){
+      context.setFillStyle(snakeHead.color);
+      context.beginPath();
+      context.rect(snakeHead.x,snakeHead.y,snakeHead.w,snakeHead.h);
+      context.closePath();
+      context.fill();
+      wx.drawCanvas({
+        canvasId:"snakeCanvas",
+        actions:context.getActions()
+      });
+      requestAnimationFrame(animate);
+    } 
+    animate();
   }
 })
